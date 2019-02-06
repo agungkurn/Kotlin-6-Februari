fade67923b8891353a1e5aea9567a4575778f18b
agungkurn <agung.excurive@live.com>
2019-02-06 18:23:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/agung/learnandroidwithkotlin/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.agung.learnandroidwithkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate")
+
+        btn_ubah.setOnClickListener {
+            tv_hello.text = "Halo, nama saya Agung!"
+        }
+    }
+
+//    fun ubahTeks(view: View) {
+//        tv_hello.text = "Halo, nama saya Agung!"
+//    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy")
+    }
+}
